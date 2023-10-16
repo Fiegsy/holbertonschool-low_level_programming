d86ff8a1b4db5668c3629520b52de183450442ae
Fiegsy <yassine.daoui031@gmail.com>
2023-10-16 08:05:13 -0700

    All tasks Done

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -13,3 +13,4 @@ int main(void)
         printf("Size of a long long int: %dbyte(s)\n", sizeof(long long int));
         printf("Size of a float: %dbyte(s)\n", sizeof(float));
         return (0);
+}
