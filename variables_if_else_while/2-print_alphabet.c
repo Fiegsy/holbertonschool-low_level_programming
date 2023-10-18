9679599dc297c2ddb906deb26d4d1918ae24e39d
Fiegsy <yassine.daoui031@gmail.com>
2023-10-18 02:30:05 -0700

    Tasks 2 is done

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * Return: 0
+ */
+
+int main(void)
+
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	putchar(i);
+	putchar('\n');
+
+	return (0);
+
+}
