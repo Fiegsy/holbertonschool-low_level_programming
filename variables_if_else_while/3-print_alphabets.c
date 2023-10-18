6dc9b35154e6f71bc531e29d96f61c59eb912f77
Fiegsy <yassine.daoui031@gmail.com>
2023-10-18 05:25:40 -0700

    Tasks 3 is done

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -9,14 +9,14 @@ int main(void)
 {
 char i;
 
-for(i= 'a'; i <= 'z'; i++)
+for (i = 'a'; i <= 'z'; i++)
 putchar(i);
 
-for(i = 'A'; i <= 'Z'; i++)
+for (i = 'A'; i <= 'Z'; i++)
 putchar(i);
 putchar('\n');
 
-return(0);
+return (0);
 
 }
 
