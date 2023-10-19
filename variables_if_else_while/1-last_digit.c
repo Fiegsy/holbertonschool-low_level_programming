6027f0027776865c56cbf123b18612fe25a68804
Fiegsy <yassine.daoui031@gmail.com>
2023-10-19 04:39:58 -0700

    Tasks 1 is done

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -5,27 +5,28 @@
 /**
  * main - annoying
  *
- * Return: 0
+ * Return: always (0)
+ *
  */
 int main(void)
 {
 	int n;
-	int m;
+	int lastd;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	m = n % 10;
-	if (m > 5)
+	lastd = n % 10;
+	if (lastd > 5)
 	{
-	printf("Last digit of %d is %d and is greater than 5\n", n, m);
+	printf("Last digit of %d is %d and is greater than 5\n", n, lastd);
 	}
-	else if (m < 6 && m != 0)
+	else if (lastd == 0)
 	{
-	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, m);
+	printf("Last digit of %d is %d and is 0\n", n, lastd);
 	}
 	else
 	{
-	printf(" Last digit of %d is %d and is 0\n", n, m);
+	printf(" Last digit of %d is %d and is less than 6 and not 0\n", n, lastd);
 	}
 	return (0);
 }
