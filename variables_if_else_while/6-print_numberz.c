c9e4d6db28f7f6e9994f901b96174583888428b4
Fiegsy <yassine.daoui031@gmail.com>
2023-10-19 02:16:06 -0700

    tasks 6 is done

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print alpha
+ *
+ * Return: always (0)
+ *
+ */
+int main(void)
+{
+	int alphabets = '0';
+
+	while (alphabets <= '9')
+	{
+		putchar (alphabets);
+	}
+	putchar ('\n');
+	return (0);
+}
