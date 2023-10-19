e0258ec042ff0e5ef04e4d7700261a3761fa50b9
Fiegsy <yassine.daoui031@gmail.com>
2023-10-19 03:14:54 -0700

    Tasks 6 is done

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *
+ * Return: always (0)
+ *
+ */
+int main(void)
+{
+	int alphabets;
+	alphabets = '0'
+
+	while (alphabets <= '9')
+	alphabets++;
+}
+        putchar(''\n);
+	return (0);
+}
