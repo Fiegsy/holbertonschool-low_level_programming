ec8da1168b533e03b20c799c65d4facb35f7e6bd
Fiegsy <yassine.daoui031@gmail.com>
2023-10-19 05:27:26 -0700

    Tasks 0 is done

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - putchar
+ *
+ * Return: (0)
+ *
+ */
+int main(void)
+{
+	int i;
+	char c[] = "_putchar";
+
+	for (i = 0; i <= 7; i++)
+	{
+		putchar(c[i];
+	}
+	putchar('\n');
+
+	return (0);
+}
