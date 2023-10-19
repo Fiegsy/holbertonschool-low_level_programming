881c8743731245816466eea29fd9290be65c98b6
Fiegsy <yassine.daoui031@gmail.com>
2023-10-19 02:02:05 -0700

    Tasks 5 is done

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
 /**
  * main - Entry p
- * Return: Always 0 (Success)
+ *
+ * Return: Always (0)
+ *
  */
 
 int main(void)
 {
-	int a;
-	for (a = 0; a < 10; a++)
-		printf"%d", a);
+	char base_ten = '0';
+
+	while (base_ten <= '9')
+	{
+		putchar(base_ten);
+		base_ten++;
+	}
 	putchar('\n');
 	return (0);
 }
