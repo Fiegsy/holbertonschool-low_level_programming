df45c630b5aeb00995a353ccdfd3d114d33e38f9
Fiegsy <yassine.daoui031@gmail.com>
2023-10-20 03:59:36 -0700

    Tasks 3 is done

---
+++ functions_nested_loops/3-main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int r;
+
+    r = islower('H');
+    _putchar(r + '0');
+    r = islower('o');
+    _putchar(r + '0');
+    r = islower(108);
+    _putchar(r + '0');
+    _putchar('\n');
+    return (0);
+}
