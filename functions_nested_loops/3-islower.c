7ae0c3a6ce342d2581ebfaa9eff6115afeb735f2
Fiegsy <yassine.daoui031@gmail.com>
2023-10-21 03:39:13 -0700

    Tasks 3 is done

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -2,16 +2,12 @@
 
 /**
  * _islower - Checks for lowercase character
- * @c: the character to be checked
- * Return: 1 for lowercase character or 0 for anything else
+ * @c: a character argument
+ * Return: returns 0 and 1 depending the context
  */
 
 int _islower(int c)
 
 {
-	if (c >= 97 && c <= 122)
-	{
-	return (1);
-	}
-	return (0);
+	return (c >= 'a' && c <= 'z');
 }
