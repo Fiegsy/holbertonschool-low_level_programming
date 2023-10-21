bac77d9f6a6d557a8dc70c867741bcd6042197a1
Fiegsy <yassine.daoui031@gmail.com>
2023-10-21 11:49:53 -0700

    Tasks 8 is done

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -5,7 +5,6 @@
 void jack_bauer(void)
 {
 	int i, j;
-
 	for (i = 0; i < 24; i++)
 	{
 		for (j = 0; j < 60; j++)
@@ -13,25 +12,24 @@ void jack_bauer(void)
 			if (i < 10)
 			{
 				_putchar('0');
-				_putchar(i + '0');
+				_putchar(i + '0')
 			}
 			else if (i >= 10)
 			{
 				_putchar((i / 10) + '0');
-				_putchar((i %  10) + '0');
+				_putchar((i % 10) + '0');
 			}
 			if (j < 10)
 			{
 				_putchar(':');
-			        _putchar('0');
+				_putchar('0');
 				_putchar(j + '0');
 			}
-		else if (j >= 10)
-		{
-			_putchar(':');
-			_putchar((j / 10) + '0');
-			_putchar((j % 10) + '0');
-		}
+		        else if (j >= 10)
+			{
+				_putchar(':');
+                                _putchar((j / 10) + '0');
+                                _putchar((j % 10) + '0');
+			}
 		_putchar('\n');
-	}
-	}
+		}
