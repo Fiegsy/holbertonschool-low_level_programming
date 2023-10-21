70ef9d2933dadd86015b499a970e214f74cfb8f6
Fiegsy <yassine.daoui031@gmail.com>
2023-10-21 03:56:16 -0700

    tasks 4 is done

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,17 +1,13 @@
 #include "main.h"
 
 /**
- * _isalpha - Checks for alphabetic character
+ * _isalpha - Checks for alphabetic letters
  * @c: The character to be checked
- * Return: 1 for alphabetic character or 0 for anything else
- * */
+ * Return: 1 or 0 depending the context
+ */
 
 int _isalpha(int c)
 
 {
-	if((c >= 65 && c <= 90) || (c= 97 && c <= 122))
-	{
-	return (1);
-	}
-	return (0);
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
