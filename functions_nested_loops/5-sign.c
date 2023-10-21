235aff6eb88924ca00335b732cbbd1f967f2c651
Fiegsy <yassine.daoui031@gmail.com>
2023-10-21 04:16:29 -0700

    tasks 5 is done

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_sign - print sign based on context
+ * @n: argument passed
+ * Return: 1, 0 -1
+ */
+int print_sign(int n)
+
+	if (n > 0)
+{
+	_putchar('+');
+	return (1);
+}
+else if (n == 0)
+{
+	_putchar('0');
+	return (0);
+}
