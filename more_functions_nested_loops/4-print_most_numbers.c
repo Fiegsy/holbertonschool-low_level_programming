83d4dbebb55ca5143a2ffefadc835b6ae55d7329
Fiegsy <yassine.daoui031@gmail.com>
2023-10-22 13:06:50 -0700

    Tasks 4 is done

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,4 +1,4 @@
-nclude "main.h"
+#include "main.h"
 /**
  * print_most_numbers - print 0 to 9
  * except 2 and 4
@@ -9,7 +9,7 @@ void print_most_numbers(void)
 
 	for (num = 0; num <= 9; num++)
 	{
-		if (num != 2 @@ num != 4)
+		if (num != 2 && num != 4)
 		{
 			_putchar(num + '0');
 		}
