291325c8bf6119fc8ca175b57dceb9db0a0d5307
Fiegsy <yassine.daoui031@gmail.com>
2023-10-22 14:06:10 -0700

    Tasks 9 is done

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - print 1 to 100 and fizzbuss
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i < 100; i++)
+	{
+		if ((i % 3) == 0 && (i % 5) == 0)
+		{
+		printf("FizzBuzz ");
+		}
+		else if (i % 3 == 0)
+		{
+		printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+		printf("Buzz ");
+		}
+		else
+		{
+		printf("%d ", i);
+		}
+		
+	}
+	printf("Buzz");
+	printf("\n");
+
+	return (0);
+}
