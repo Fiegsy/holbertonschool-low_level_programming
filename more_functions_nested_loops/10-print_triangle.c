6f9e23ebe8478d7acd4aff021f9385b377f67d7a
Fiegsy <yassine.daoui031@gmail.com>
2023-10-22 14:18:01 -0700

    Tasks 10 is done

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * print_triangle - print a triangle
+ * @size: the size of a triangle
+ */
+void print_triangle(int size)
+{
+	inti, j;
+
+	if (size > 0)
+	{
+	for (i = 0, i < size; i++)
+	{
+	for (j = 0; j < size; j++)
+	{
+	if (j < size - (i + 1))
+	{
+		_putchar(' ');
+	}
+	else
+	{
+		_putchar('#');
+	}
+	}
+	_putchar('\n');
+	}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
