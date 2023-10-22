093836707ffaa1796a578ca9431767245a4ca063
Fiegsy <yassine.daoui031@gmail.com>
2023-10-22 11:53:35 -0700

    Tasks 11 is done

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 /**
- * Print_to_98 - Prints all natural numbers from n to 98
+ * print_to_98 - Prints all natural numbers from n to 98
  * @n: The number to start printing from
  * Return: Always 0.
  */
