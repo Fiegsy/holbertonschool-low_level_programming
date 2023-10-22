2d95431dcb6cad9e3984cc2732dbbe03ae801108
Fiegsy <yassine.daoui031@gmail.com>
2023-10-22 13:41:55 -0700

    Tasks 8 is done

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -12,7 +12,7 @@ void print_square(int size)
 	{
 		for (i = 0; i < size; i++)
 		{
-			for (j + 0; j < size; j++)
+			for (j = 0; j < size; j++)
 			{
 			_putchar('#');
 			}
@@ -23,3 +23,4 @@ void print_square(int size)
 	{
 	_putchar('\n');
 	}
+}
