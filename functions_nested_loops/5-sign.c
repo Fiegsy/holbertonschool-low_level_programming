f7182303e7d8078132b088138adb6f4352763d6d
Fiegsy <yassine.daoui031@gmail.com>
2023-10-22 06:05:03 -0700

    Tasks 5 is done

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -8,22 +8,21 @@
  */
 
 int print_sign(int n)
-{
-	int negative = -1;
-	char ch = (char) negative;
 
+{
 	if (n > 0)
-{
-	_putchar('+');
-	return (1);
-}
-else if (n == 0)
-{
-	_putchar('0');
-	return (0);
-}
-else
-{
-	_putchar('-');
-	return (ch);
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar (45);
+		return (-1);
+	}
+	else
+	{
+		_putchar (48);
+		return (0);
+	}
 }
