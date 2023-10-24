8f2b4e50faea02da40e9d51e75b8f2fc12bcd1e4
Fiegsy <yassine.daoui031@gmail.com>
2023-10-24 06:39:17 -0700

    Tasks 8 is over

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -3,7 +3,7 @@
 /**
  * print_array - prints n elements of an array of integers
  * @a: array
- * @n: number of elements
+ * @n: number of elements to be printed
  * Return: Void
  */
 
@@ -11,7 +11,7 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
+	for (i = 0 ; i < n ; i++)
 {
 	printf("%d", a[i]);
 			if (i < n - 1)
