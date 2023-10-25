755b84e550936c02f60619fb6af2866369154b6f
Fiegsy <yassine.daoui031@gmail.com>
2023-10-25 06:25:53 -0700

    Tasks 9 is done

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * char *_strcpy - a function that copies the string pointed
+ * @dest: copy to
+ * @src: copy from
+ * Return: string
+ */
+ char *_strcpy(char *dest, char *src)
+{
+        int i = 0;
+        int x = 0;
+
+        while (*(src + i) != '\0')
+        {
+                i++;
+        }
+        for ( ; x < i ; x++)
+        {
+                dest[x] = src[x];
+        }
+        dest[i] = '\0';
+        return (dest);
+}
+
