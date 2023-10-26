44786c3b18bad44ad1484aea7edda4e63627a4ec
Fiegsy <yassine.daoui031@gmail.com>
2023-10-26 05:05:42 -0700

    Tasks 3 is done

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -16,5 +16,6 @@ char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
 
 #endif
