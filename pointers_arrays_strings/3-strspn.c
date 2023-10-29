d24e53e3d608fe869bff6e1219f0f4ffcc8f6105
Fiegsy <yassine.daoui031@gmail.com>
2023-10-29 06:15:19 -0700

    Tasks 6 is done

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,8 @@
+#include "main.h"
+/**
+ * _strspn - Entry P
+ * @s: input
+ * @accept: input
+ * Return: Always 0 (Success)
+ */
+unsigned int _strspn(char *s, char *accept
--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -11,6 +11,11 @@ char *cap_string(char *str)
 
 	while (str[index])
 	{
-		while(.(str[index] >= 'a' && str[index] <= 'z'))
+		while(!(str[index] >= 'a' && str[index] <= 'z'))
 			index++;
-		if (str[index - 1] == '' || str[index - 1] == '\t' || str[index - 1] == '\n' ||
+		if (str[index - 1] == ' ' || str[index - 1] == '\t' || str[index - 1] == '\n' || str[index - 1] == ',' || str[index - 1] == ';' || str[index - 1] == '.' || str[index - 1] == '!' || str[index - 1] == '?' || str[index - 1] == '"' || str[index - 1] == '(' || str[index - 1] == ')' || str[index - 1] == '{' || str[index - 1] == '}' || index == 0)
+			str[index] -= 32;
+		index++;
+	}
+	return (str);
+}
