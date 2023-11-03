82d7978eddc1dc5bea8a35965b04f7e5f6d7f34d
Fiegsy <yassine.daoui031@gmail.com>
2023-11-03 02:30:11 -0700

    Tasks 0 is finished

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -4,10 +4,9 @@
  *
  * @argc: number of command line arguments
  * @argv: an array that contains the command line argument
- *
- * Return: (0) 
+ * Return: (0)
  */
-int main(int agrc __attribute__((unused)), char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
