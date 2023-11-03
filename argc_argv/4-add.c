0ccefc717a3a2cb0aa4983b8007fbf3893dfd142
Fiegsy <yassine.daoui031@gmail.com>
2023-11-03 04:03:54 -0700

    Tasks 4 is finish

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -13,9 +13,9 @@ int main(int argc, char *argv[])
 
 	for (i = 1; i < argc; i++)
 	{
-		for (j = 0; argv[i] [j] != '\0'; j++)
+		for(j = 0; argv[i] [j] != '\0'; j++)
 		{
-			if (!isdigit(argv[i] [j]))
+			if(!isdigit(argv[i] [j]))
 			{
 				printf("Error\n");
 				return (1);
