0576a679186066a62efe7a511011427c2823369d
Fiegsy <yassine.daoui031@gmail.com>
2023-11-03 02:54:00 -0700

    Tasks 2 is done

---
+++ argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print all arguments it receive
+ * @argc: number of line arg
+ * @argv: array that contains the program command line
+ * Return: (0)
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
+ 
