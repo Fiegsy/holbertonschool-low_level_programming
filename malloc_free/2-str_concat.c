d07350ba183920bfa0526ce204b7b46600cf07c6
Fiegsy <yassine.daoui031@gmail.com>
2023-11-08 03:34:46 -0800

    Tasks 2 is done

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -26,3 +26,15 @@ char *str_concat(char *s1, char *s2)
 		while (s2[i++] != '\0')
 			len2++;
 	}
+
+	len = len1 + len2;
+	result = (char *)malloc(sizeof(char) * (len + 1));
+	if (result == NULL)
+		return (NULL);
+	for (i = 0; i < len1; i++)
+		result[i] = s1[i];
+	for (j = 0; j < len2; j++, i++)
+		result[i] = s2[j];
+	result[len] = '\0';
+	return (result);
+}
