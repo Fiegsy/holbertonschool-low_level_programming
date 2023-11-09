d62846cf94438e2d883cef842ac8d36d909aa7be
Fiegsy <yassine.daoui031@gmail.com>
2023-11-09 06:17:19 -0800

    Tasks 2 is dumb

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -12,7 +12,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	char *d;
 	unsigned int b;
 
-	if(nmemb == 0 || size == 0)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
 	d = malloc(nmemb * size);
 	if (d == NULL)
