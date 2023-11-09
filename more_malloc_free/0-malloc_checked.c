3afd7fa6b939fbe87ef479f867441c81360167a6
Fiegsy <yassine.daoui031@gmail.com>
2023-11-09 04:32:03 -0800

    Tasks 0 is done

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * malloc_checked - cause normal process termination with a status value of 98
+ *
+ * @b: allocated memory
+ *
+ * Return: a pointer to the allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *m;
+
+	m =  malloc(b);
+       if (m == NULL)
+               exit(98);
+        return (m);
+}	
---
+++ more_malloc_free/main.h
@@ -0,0 +1,22 @@
+#ifndef _MAIN_
+#define _MAIN_
+
+int _putchar(char c);
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+
+
+
+
+
+
+
+#endif
