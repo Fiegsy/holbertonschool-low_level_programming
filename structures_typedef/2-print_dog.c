91c9d6e9aedfc6573fb1f7770950fd1b14e2ae2d
Fiegsy <yassine.daoui031@gmail.com>
2023-11-13 07:27:21 -0800

    Tasks 2 is gone

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -16,7 +16,7 @@ void print_dog(struct dog *d)
 		else
 			printf("Name: (nil)\n");
 		printf("Age: %.6f\n", d->age);
-		if(d->owner != NULL)
+		if (d->owner != NULL)
 			printf("Owner: %s\n", d->owner);
 		else
 			printf("Owner: (nil)\n");
