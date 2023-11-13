a0541c132fd93fdadc3e2219939b4782fc0e1df4
Fiegsy <yassine.daoui031@gmail.com>
2023-11-13 07:21:37 -0800

    Tasks 2 is over

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - prints the structure dog
+ * @d: pointer to the structure
+ *
+ * Return: void
+ *
+ */
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		if (d->name != NULL)
+			printf("Name: %s\n", d->name);
+		else
+			printf("Name: (nil)\n");
+		printf("Age: %.7f\n", d->age);
+		if(d->owner != NULL)
+			printf("Owner: %s\n", d->owner);
+		else
+			printf("Owner: (nil)\n");
+	}
+}
