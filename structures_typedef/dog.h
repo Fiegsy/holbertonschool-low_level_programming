22466500766acb334fe5e4be446064e51fb08d2c
Fiegsy <yassine.daoui031@gmail.com>
2023-11-13 02:04:05 -0800

    Tasks 0 is done

---
+++ structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG
+#define DOG
+/**
+ * struct dog - dog cast
+ *
+ * @name: name of the dog
+ *
+ * @age: dog's age
+ *
+ * @owner: owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
+	
