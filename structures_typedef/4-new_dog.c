7677b73ed2983e6c24afd9c93c011fa95328f734
Fiegsy <yassine.daoui031@gmail.com>
2023-11-14 05:51:51 -0800

    Tasks 4 is done

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * new_dog - creates new dog
+ * @name: names's dog
+ * @age: dog's age
+ * @owner: owner's name
+ *
+ * Return: pointer to dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	unsigned int n1, o1, i;
+	dog_t *dog;
+
+	if (name == NULL || owner == NULL)
+		return (NULL);
+	dog = malloc(sizeof(dog_t));
+	if (dog ==  NULL)
+		return (NULL);
+	for (n1 = 0; name[n1]; n1++)
+		;
+	n1++;
+	dog->name = malloc(n1 * sizeof(char));
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+	for (i = 0; i < n1; i++)
+		dog->name[i] = name[i];
+	dog->age = age;
+	for (o1 = 0; owner[o1]; o1++)
+		;
+	o1++;
+	dog->owner = malloc(o1 * sizeof(char));
+	if (dog->owner == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+	for (i = 0; i < o1; i++)
+		dog->owner[i] = owner[i];
+	return (dog);
+}
