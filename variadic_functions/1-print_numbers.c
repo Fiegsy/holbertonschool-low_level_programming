d6230f43c0ef88be3a3472de818eb73f603438d7
Fiegsy <yassine.daoui031@gmail.com>
2023-11-16 07:12:24 -0800

    tasks 1 is finished

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -19,7 +19,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		printf("%d", va_arg(nums, int));
 
-		if (index != (n -1) && separator != NULL)
+		if (index != (n - 1) && separator != NULL)
 			printf("%s", separator);
 	}
 	printf("\n");
