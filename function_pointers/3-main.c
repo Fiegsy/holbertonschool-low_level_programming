327e36129bbc6111c39c416fce2f2a9f1100ce0a
Fiegsy <yassine.daoui031@gmail.com>
2023-11-17 14:06:00 -0800

    Tasks Calculator

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -16,3 +16,21 @@ int main(int argc, char *argv[])
 		printf("Error:n");
 		exit(98);
 	}
+	if (argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	s = argv[2][0];
+	if (s != '+' && s != '_' && s != '/' && s != '*' && s != '%')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	res = (get_op_func(argv[2]))(a, b);
+	printf("%d\n, res);
+	return (0);
+}
