b2fea96303ce73a4740f21993aaae0debe39547b
Fiegsy <yassine.daoui031@gmail.com>
2023-11-28 06:26:03 -0800

    Added a modif on file list

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -12,7 +12,7 @@ size_t print_list(const list_t *h)
 	nelem = 0;
 	while (h != NULL)
 	{
-		if(h->str == NULL)
+		if (h->str == NULL)
 			printf("[%d] %s\n", 0, "(nil)");
 		else
 			printf("[%d] %s\n", h->len, h->str);
