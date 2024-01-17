b7132ce1ff14c7230f8f3bc8e2951e8267c6dd09
Fiegsy <yassine.daoui031@gmail.com>
2024-01-17 15:03:02 -0800

    Fixed betty issue

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -12,17 +12,17 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-    if (array == NULL)
-        return -1;
+	if (array == NULL)
+		return (-1);
 
-    for (size_t i = 0; i < size; i++)
-    {
-        printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+	for (size_t i = 0; i < size; i++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 
-        if (array[i] == value)
-            return (int)i;
-    }
+		if (array[i] == value)
+			return (int i);
+	}
 
-    return -1;
+	return (-1);
 }
 
