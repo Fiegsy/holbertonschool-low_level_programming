6b2e1f72073c65e40406fe34819e8198ca9deb85
Fiegsy <yassine.daoui031@gmail.com>
2024-01-17 14:55:49 -0800

    Created files for task 0

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,28 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - Search a value in an array of integers using linear search
+ * algorithm
+ *
+ * @array: Array to be searched
+ * @size: Size of the array
+ * @value: Value to look for
+ *
+ * Return: The index of value in the array, or -1 if not found
+ */
+int linear_search(int *array, size_t size, int value)
+{
+    if (array == NULL)
+        return -1;
+
+    for (size_t i = 0; i < size; i++)
+    {
+        printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+
+        if (array[i] == value)
+            return (int)i;
+    }
+
+    return -1;
+}
+
