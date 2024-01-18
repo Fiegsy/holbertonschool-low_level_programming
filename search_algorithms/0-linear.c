50edb7f767ac6eaf39276fc06c24b56e417f0181
Fiegsy <yassine.daoui031@gmail.com>
2024-01-18 02:03:59 -0800

    file ok in local

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -22,7 +22,7 @@ int linear_search(int *array, size_t size, int value)
 		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 
 		if (array[i] == value)
-			return (i(int));
+			return (i);
 	}
 
 	return (-1);
